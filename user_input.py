1edac10643512b583e5df7cc6788af0f047e6694
FredrickOsambo <160466283+FredrickOsambo@users.noreply.github.com>
2024-02-22 08:41:32 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,7 +1,1 @@
-name = input("what is your name: ")
-age = input("what is your age: ")
-location = input("what is your location: ")
-
-print("Hello" +name+ ", you are "+age+"years old and live in" + location )
-
 
